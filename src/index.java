50d8fd2e8801133fd7114203c8e78304230afa5a
Stephen Guedea <guedea.stephen@gmail.com>
2018-11-26 15:23:07 -0600

    added add contacts method

--- src/index.java
+++ src/index.java
@@ -4,6 +4,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.nio.file.StandardOpenOption;
 
@@ -22,6 +23,17 @@ public class index {
 
     }
 
+    // ADD CONTACTS
+    public static void addContact(Path p, List<String> newContact){
+        try{
+            Files.write(p, newContact, StandardOpenOption.APPEND);
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
 
 //    MAIN
     public static void main(String[] args){
@@ -29,8 +41,10 @@ public class index {
         Path p = Paths.get("contacts.txt");
         List<String> file = new ArrayList<>();
 
+//        showContacts(p, file);
+
+        List<String> newContact = Arrays.asList("John Smith 2105555555");
+        addContact(p, newContact);
         showContacts(p, file);
-
-
     }
 }
